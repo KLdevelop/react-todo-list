97f880ae21d854674a258da7e4e5f3fb64a0579a
KLdevelop <drapdrop1@gmail.com>
2023-08-06 11:44:53 +0300

    clickable todo item

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -4,17 +4,22 @@ import { TodoItem, TodoItems } from 'src/types';
 
 interface TodoListProps {
   todoItems: TodoItems;
-  activeId?: number;
-  setActiveId?: () => void;
+  activeId: number | null;
+  setActiveId: (id: number | null) => void;
 }
 
 export const TodoList = (props: TodoListProps) => {
   const todoItems = Object.entries<TodoItem>(props.todoItems);
+  const { activeId, setActiveId } = props;
 
   return (
     <div className={styles.todoList}>
       {todoItems.map(([id, todoItem]) => (
-        <div key={id}>
+        <div
+          className={activeId === +id ? styles.activeTodoItem : styles.todoItem}
+          key={id}
+          onClick={() => setActiveId(Number(id))}
+        >
           <p>{todoItem.title}</p>
           <p>{todoItem.status}</p>
         </div>
