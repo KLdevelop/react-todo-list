5731444b27f9f53208aaca70742220ff8fb7c041
KLdevelop <drapdrop1@gmail.com>
2023-08-07 15:48:02 +0300

    new map instead old

--- src/reducers/todoReducer/todoReducer.ts
+++ src/reducers/todoReducer/todoReducer.ts
@@ -5,17 +5,26 @@ import { TodoAction, TodoActions } from './types';
 export const todoReducer: Reducer<TodoItems, TodoAction> = (state, action) => {
   switch (action.type) {
     case TodoActions.SET_TODO: {
-      state.set(action.payload.id, action.payload.todoItem);
-      return new Map(state);
+      const newState = new Map(state);
+
+      newState.set(action.payload.id, action.payload.todoItem);
+
+      return newState;
     }
+
     case TodoActions.ADD_TODO: {
-      const ids = Object.keys(state);
+      const ids = Array.from(state.keys());
       const idCount = ids.length;
       const newID = idCount > 0 ? Number(ids[idCount - 1]) + 1 : 0;
+      console.log(newID);
 
-      state.set(newID, action.payload);
-      return new Map(state);
+      const newState = new Map(state);
+
+      newState.set(newID, action.payload);
+
+      return newState;
     }
+
     default: {
       return state;
     }
