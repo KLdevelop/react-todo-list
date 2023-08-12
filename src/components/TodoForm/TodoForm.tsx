bc1a70d25c382cd56e4b52c16ee2bef31fcdc66f
KLdevelop <drapdrop1@gmail.com>
2023-08-12 13:23:54 +0300

    todo search

--- src/components/TodoForm/TodoForm.tsx
+++ src/components/TodoForm/TodoForm.tsx
@@ -2,9 +2,13 @@ import React, { useState } from 'react';
 import styles from './todoForm.module.scss';
 import { TodoList, TodoEditor } from '..';
 import { useCacheableTodoState, useResizer } from 'src/hooks';
+import { filterTodoList } from 'src/utils';
 
 export const TodoForm = () => {
   const { todoItems, setTodoItem, addTodoItem, deleteTodoItem } = useCacheableTodoState();
+  const [filterString, setFilterString] = useState('');
+  const filteredTodoItems = filterTodoList(todoItems, filterString);
+
   const [activeId, setActiveId] = useState<number | null>(null);
   const activeItem = activeId !== null ? todoItems.get(activeId) : undefined;
 
@@ -16,10 +20,17 @@ export const TodoForm = () => {
         <button onClick={addTodoItem} className={styles.addTodoItem}>
           Добавить задачу
         </button>
+        <input
+          className={styles.search}
+          type="text"
+          value={filterString}
+          onChange={(e) => setFilterString(e.target.value)}
+          placeholder="Поиск задачи"
+        />
       </div>
       <div className={styles.content}>
         <span className={styles.todoList} ref={resizableRef}>
-          <TodoList todoItems={todoItems} activeId={activeId} setActiveId={setActiveId} />
+          <TodoList todoItems={filteredTodoItems} activeId={activeId} setActiveId={setActiveId} />
         </span>
         <span className={styles.resizer} ref={resizerRef} />
         {activeId !== null && activeItem !== undefined && (
