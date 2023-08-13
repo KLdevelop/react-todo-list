ff1601d36018e66acf0db502a7088bd87d25d2e4
KLdevelop <drapdrop1@gmail.com>
2023-08-13 13:48:06 +0300

    commented types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,11 @@
+/** Статус задачи из списка дел. */
 export type TodoItemStatus = 'waiting' | 'in process' | 'completed';
 
+/** Задача из списка дел. */
 export interface TodoItem {
   title: string;
   status: TodoItemStatus;
 }
 
+/** Список дел. */
 export type TodoItems = Map<number, TodoItem>;
